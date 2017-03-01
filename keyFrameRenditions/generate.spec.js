be59ed1393b16e4893389a4395e5a2ba0b3bbc55
Charlie Dibble <cgdibble@gmail.com>
2017-03-01 08:08:58 -0800

    generate needs tests and actual functionality around running ffprobe from the node process

--- keyFrameRenditions/generate.spec.js
+++ keyFrameRenditions/generate.spec.js
@@ -3,5 +3,5 @@ const { isSuccess, isFailure } = require('consistent-failables/failable')
 const { isolateIFrames, isIFrame, findVideoLength } = require('./generate')
 
 describe('generate.js', () => {
-  it('doesnt have a damn test')
+  it('doesnt have a damn test yet')
 })
